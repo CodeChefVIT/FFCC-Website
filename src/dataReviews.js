85a9e8239be277b33fdf04fd649e10ff44c7526b
jugalbhatt123 <jugalbhatt3@gmail.com>
2020-04-19 16:43:23 +0530

    init Reviews updated

---
+++ src/dataReviews.js
@@ -0,0 +1,429 @@
+const data =  [
+    {
+      "SCHOOL": "SMEC",
+      "NAME": "ANIL P.M.",
+      "RATINGS": 3
+    },
+    {
+      "SCHOOL": "SMEC",
+      "NAME": "ANOJ GIRI",
+      "RATINGS": 3
+    },
+    {
+      "SCHOOL": "SMEC",
+      "NAME": "SENTHILKUMAR P",
+      "RATINGS": 3
+    },
+    {
+      "SCHOOL": "SMEC",
+      "NAME": "BIBHUTI BHUSAN SAHOO",
+      "RATINGS": 3
+    },
+    {
+      "SCHOOL": "SMEC",
+      "NAME": "ARUNA KUMAR BEHURA",
+      "RATINGS": 2
+    },
+    {
+      "SCHOOL": "SMEC",
+      "NAME": "EDWIN SUDHAGAR P",
+      "RATINGS": 2
+    },
+    {
+      "SCHOOL": "SMEC",
+      "NAME": "THIAGARAJAN S",
+      "RATINGS": 2
+    },
+    {
+      "SCHOOL": "SMEC",
+      "NAME": "RAHUL SINGH SIKARWAR",
+      "RATINGS": 1
+    },
+    {
+      "SCHOOL": "SMEC",
+      "NAME": "NAIJU C D",
+      "RATINGS": 1
+    },
+    {
+      "SCHOOL": "SITE",
+      "NAME": "SWARNA PRIYA R.M",
+      "RATINGS": 3
+    },
+    {
+      "SCHOOL": "SITE",
+      "NAME": "SANJIBAN SEKHAR ROY",
+      "RATINGS": 2
+    },
+    {
+      "SCHOOL": "SITE",
+      "NAME": "KAVITHA B.R",
+      "RATINGS": 2
+    },
+    {
+      "SCHOOL": "SITE",
+      "NAME": "MYTHILI N",
+      "RATINGS": 1
+    },
+    {
+      "SCHOOL": "SENSE",
+      "NAME": "NITHISH KUMAR V",
+      "RATINGS": 3
+    },
+    {
+      "SCHOOL": "SENSE",
+      "NAME": "SASIKUMAR P",
+      "RATINGS": 3
+    },
+    {
+      "SCHOOL": "SENSE",
+      "NAME": "ARUNKUMAR CHANDRASEKHAR",
+      "RATINGS": 3
+    },
+    {
+      "SCHOOL": "SENSE",
+      "NAME": "CHRISTOPHER CLEMENT J",
+      "RATINGS": 3
+    },
+    {
+      "SCHOOL": "SENSE",
+      "NAME": "VAEGAE NAVEEN KUMAR",
+      "RATINGS": 3
+    },
+    {
+      "SCHOOL": "SENSE",
+      "NAME": "NITHISH KUMAR V",
+      "RATINGS": 2
+    },
+    {
+      "SCHOOL": "SENSE",
+      "NAME": "VAEGAE NAVEEN KUMAR",
+      "RATINGS": 2
+    },
+    {
+      "SCHOOL": "SENSE",
+      "NAME": "VENUGOPAL P",
+      "RATINGS": 2
+    },
+    {
+      "SCHOOL": "SENSE",
+      "NAME": "SATHYA P",
+      "RATINGS": 2
+    },
+    {
+      "SCHOOL": "SCOPE",
+      "NAME": "BALAKRISHNAN P",
+      "RATINGS": 3
+    },
+    {
+      "SCHOOL": "SCOPE",
+      "NAME": "NARESH K",
+      "RATINGS": 3
+    },
+    {
+      "SCHOOL": "SCOPE",
+      "NAME": "JANANI T",
+      "RATINGS": 3
+    },
+    {
+      "SCHOOL": "SCOPE",
+      "NAME": "SURAJ SHANKARLAL MEGHWANI",
+      "RATINGS": 3
+    },
+    {
+      "SCHOOL": "SCOPE",
+      "NAME": "VISHNU SRINIVA MURTHY Y",
+      "RATINGS": 3
+    },
+    {
+      "SCHOOL": "SCOPE",
+      "NAME": "KRUPA S",
+      "RATINGS": 3
+    },
+    {
+      "SCHOOL": "SCOPE",
+      "NAME": "ARUP GHOSH",
+      "RATINGS": 3
+    },
+    {
+      "SCHOOL": "SCOPE",
+      "NAME": "VELLINGIRI J",
+      "RATINGS": 3
+    },
+    {
+      "SCHOOL": "SCOPE",
+      "NAME": "VIJAYA KUMAR K",
+      "RATINGS": 3
+    },
+    {
+      "SCHOOL": "SCOPE",
+      "NAME": "DHEEBA J",
+      "RATINGS": 3
+    },
+    {
+      "SCHOOL": "SCOPE",
+      "NAME": "SRIVANI A",
+      "RATINGS": 3
+    },
+    {
+      "SCHOOL": "SCOPE",
+      "NAME": "VAIRAMUTHU S",
+      "RATINGS": 3
+    },
+    {
+      "SCHOOL": "SCOPE",
+      "NAME": "BALAMURUGAN R",
+      "RATINGS": 3
+    },
+    {
+      "SCHOOL": "SCOPE",
+      "NAME": "ANAND M",
+      "RATINGS": 3
+    },
+    {
+      "SCHOOL": "SCOPE",
+      "NAME": "BOOMINATHAN P",
+      "RATINGS": 3
+    },
+    {
+      "SCHOOL": "SCOPE",
+      "NAME": "SURESHKUMAR N",
+      "RATINGS": 3
+    },
+    {
+      "SCHOOL": "SCOPE",
+      "NAME": "PARVEEN SULTANA H",
+      "RATINGS": 3
+    },
+    {
+      "SCHOOL": "SCOPE",
+      "NAME": "VELLINGIRI J",
+      "RATINGS": 3
+    },
+    {
+      "SCHOOL": "SCOPE",
+      "NAME": "ANBARASI M",
+      "RATINGS": 3
+    },
+    {
+      "SCHOOL": "SCOPE",
+      "NAME": "ASHWIN GANESAN",
+      "RATINGS": 3
+    },
+    {
+      "SCHOOL": "SCOPE",
+      "NAME": "DHEEBA J",
+      "RATINGS": 3
+    },
+    {
+      "SCHOOL": "SCOPE",
+      "NAME": "GOPALAKRISHNAN T",
+      "RATINGS": 3
+    },
+    {
+      "SCHOOL": "SCOPE",
+      "NAME": "MOHAN K",
+      "RATINGS": 3
+    },
+    {
+      "SCHOOL": "SCOPE",
+      "NAME": "PADMA PRIYA R",
+      "RATINGS": 3
+    },
+    {
+      "SCHOOL": "SCOPE",
+      "NAME": "VIJAYARANI A",
+      "RATINGS": 3
+    },
+    {
+      "SCHOOL": "SCOPE",
+      "NAME": "ANURADHA G",
+      "RATINGS": 2
+    },
+    {
+      "SCHOOL": "SCOPE",
+      "NAME": "SATHIYA KUMAR C",
+      "RATINGS": 2
+    },
+    {
+      "SCHOOL": "SCOPE",
+      "NAME": "GAYATHRI P",
+      "RATINGS": 2
+    },
+    {
+      "SCHOOL": "SCOPE",
+      "NAME": "AKILA VICTOR",
+      "RATINGS": 2
+    },
+    {
+      "SCHOOL": "SCOPE",
+      "NAME": "RAMANATHAN L",
+      "RATINGS": 2
+    },
+    {
+      "SCHOOL": "SCOPE",
+      "NAME": "GOVINDA K",
+      "RATINGS": 2
+    },
+    {
+      "SCHOOL": "SCOPE",
+      "NAME": "NEELU KHARE",
+      "RATINGS": 2
+    },
+    {
+      "SCHOOL": "SCOPE",
+      "NAME": "NAVAMANI T M",
+      "RATINGS": 2
+    },
+    {
+      "SCHOOL": "SCOPE",
+      "NAME": "ABDUL GAFFAR H",
+      "RATINGS": 2
+    },
+    {
+      "SCHOOL": "SCOPE",
+      "NAME": "BHULAKSHMI BONTHU",
+      "RATINGS": 2
+    },
+    {
+      "SCHOOL": "SCOPE",
+      "NAME": "VIJAYAN E",
+      "RATINGS": 2
+    },
+    {
+      "SCHOOL": "SCOPE",
+      "NAME": "ARUN KUMAR G",
+      "RATINGS": 2
+    },
+    {
+      "SCHOOL": "SCOPE",
+      "NAME": "RAMANATHAN L",
+      "RATINGS": 2
+    },
+    {
+      "SCHOOL": "SCOPE",
+      "NAME": "SARAVANAGURU RA.K",
+      "RATINGS": 2
+    },
+    {
+      "SCHOOL": "SCOPE",
+      "NAME": "KAUSER AHMED P",
+      "RATINGS": 2
+    },
+    {
+      "SCHOOL": "SCOPE",
+      "NAME": "SAIRABANU J",
+      "RATINGS": 2
+    },
+    {
+      "SCHOOL": "SCOPE",
+      "NAME": "SENDHIL KUMAR K.S",
+      "RATINGS": 2
+    },
+    {
+      "SCHOOL": "SCOPE",
+      "NAME": "GAYATHRI P",
+      "RATINGS": 2
+    },
+    {
+      "SCHOOL": "SCOPE",
+      "NAME": "RUBY D",
+      "RATINGS": 2
+    },
+    {
+      "SCHOOL": "SCOPE",
+      "NAME": "SELVI M",
+      "RATINGS": 1
+    },
+    {
+      "SCHOOL": "SCOPE",
+      "NAME": "CHANDRASEGAR.T",
+      "RATINGS": 1
+    },
+    {
+      "SCHOOL": "SCOPE",
+      "NAME": "ANURADHA G",
+      "RATINGS": 1
+    },
+    {
+      "SCHOOL": "SCHEME",
+      "NAME": "MAHESH GANESAPILLAI",
+      "RATINGS": 3
+    },
+    {
+      "SCHOOL": "SAS",
+      "NAME": "SENTHUR PANDI R",
+      "RATINGS": 3
+    },
+    {
+      "SCHOOL": "SAS",
+      "NAME": "THIRUMOORTHY K.",
+      "RATINGS": 3
+    },
+    {
+      "SCHOOL": "SAS",
+      "NAME": "ANAND PRABU A",
+      "RATINGS": 3
+    },
+    {
+      "SCHOOL": "SAS",
+      "NAME": "MADHU V",
+      "RATINGS": 3
+    },
+    {
+      "SCHOOL": "SAS",
+      "NAME": "VENKATA SATYANARAYANA",
+      "RATINGS": 3
+    },
+    {
+      "SCHOOL": "SAS",
+      "NAME": "ROY.S",
+      "RATINGS": 3
+    },
+    {
+      "SCHOOL": "SAS",
+      "NAME": "EZHIL VIZHI R",
+      "RATINGS": 3
+    },
+    {
+      "SCHOOL": "SAS",
+      "NAME": "MANJU S L",
+      "RATINGS": 3
+    },
+    {
+      "SCHOOL": "SAS",
+      "NAME": "MELLACHERUVU NAGA SRINIVASU",
+      "RATINGS": 3
+    },
+    {
+      "SCHOOL": "SAS",
+      "NAME": "RAJAN BABU D",
+      "RATINGS": 3
+    },
+    {
+      "SCHOOL": "SAS",
+      "NAME": "RAJESH BHATT",
+      "RATINGS": 2
+    },
+    {
+      "SCHOOL": "SAS",
+      "NAME": "SUBRAMANYAM REDDY",
+      "RATINGS": 2
+    },
+    {
+      "SCHOOL": "SAS",
+      "NAME": "RAJASEKAR P",
+      "RATINGS": 2
+    },
+    {
+      "SCHOOL": "SAS",
+      "NAME": "RAMESH KUMAR D",
+      "RATINGS": 2
+    },
+    {
+      "SCHOOL": "SAS",
+      "NAME": "RAJENDRAN P",
+      "RATINGS": 1
+    }
+  ]
+
+module.exports =data
--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 const express = require('express')
-
-const User = require('./models/user')
+var async = require('async');
 const { MongoClient, ObjectID } = require('mongodb')
 const app = express()
 const port = process.env.PORT || 3000
- const mongoose = require('mongoose')
+const mongoose = require('mongoose')
 app.use(express.json())
+const data = require('./dataReviews')
+
+
+console.log(data.length)
 
 
 // DataBase Config
@@ -28,7 +31,7 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
           code =req.params.code
         db.collection('user').find({ CODE: code }).toArray((error, result) => {
             res.send(result)
-            
+           
         })
     })
     app.patch('/time',(req,res)=>{
@@ -52,7 +55,7 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
         })
         if(flag===1){
             app.get('/time',async(req,res)=>{
-                db.collection('user').find({FACULTY:result[0].FACULTY,CODE:result[0].CODE,SLOT:{$regex:/^L3[0-9]|^L4[0-9]|^L5[[0-9]/}}).toArray((error,result)=>{
+                db.collection('user').find({FACULTY:name,CODE:code,SLOT:{$regex:/^L3[0-9]|^L4[0-9]|^L5[[0-9]/}}).toArray((error,result)=>{
                     console.log(result)
                     res.send(result)
                 })
@@ -61,7 +64,7 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
         }
         else{
             app.get('/time',async(req,res)=>{
-                db.collection('user').find({FACULTY:result[0].FACULTY,CODE:result[0].CODE,SLOT:{$regex:/^L[0-9]|^L1[0-9]|^L2[0-9]/}}).toArray((error,result)=>{
+                db.collection('user').find({FACULTY:name,CODE:code,SLOT:{$regex:/^L[0-9]|^L1[0-9]|^L2[0-9]/}}).toArray((error,result)=>{
                     console.log(result)
                     res.send(result)
                 })
@@ -72,6 +75,41 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
         
 
     })
+
+    
+
+    async.eachSeries(data, function updateObject (obj, done) {
+        // Model.update(condition, doc, callback)
+        db.collection('user').updateMany({ FACULTY: obj.NAME }, { $set : { REVIEW: obj.RATINGS }}, done);
+    }, function allDone (err) {
+        // this will be called when all the updates are done or an error occurred during the iteration
+    });
+
+    
+    // mr = db.runCommand({
+    //     "mapreduce": "user",
+    //     "map": function() {
+    //         var regxp = /\s/;
+    //         for (var key in this) { 
+    //             if (key.match(regxp)) {
+    //                 emit(key, null); 
+    //             }
+    //         }
+    //     },
+    //     "reduce": function() {}, 
+    //     "out": "filtered_keys"
+    // })
+    
+    // db[mr.result].distinct("_id")
+    
+    // var update = { "$rename": {} };
+    // db[mr.result].distinct("_id").forEach(function (key){
+    //     update["$rename"][key] = key.replace(/\s+/g, "_");
+    // });
+    
+    // //print(update)
+    
+    // db.user.update({ }, update, false, true );
     
         
 
