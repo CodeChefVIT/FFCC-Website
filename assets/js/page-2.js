05bf7aca20a182047c30668fa7e10f91d611486d
jugalbhatt123 <jugalbhatt3@gmail.com>
2020-05-13 16:04:45 +0530

    fixed broken links and added page-2.js

---
+++ assets/js/page-2.js
@@ -0,0 +1,14 @@
+
+function addDropdowns(){
+var number = document.getElementById('numOfSubjects').value
+console.log(number)
+if(number===1){
+    $('#subject-2').hide()
+    console.log('hidden 1')
+    $('#subject-3').hide()
+    $('#subject-4').hide()
+    $('#subject-5').hide()
+    $('#subject-6').hide()
+
+}
+}
